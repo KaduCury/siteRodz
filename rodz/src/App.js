da902aeb6d7fb7a35fca850f7bedf110c864d62c
Kadu Cury <carlosercury@gmail.com>
2023-04-05 16:04:22 -0300

    first commit

---
+++ rodz/src/App.js
@@ -0,0 +1,68 @@
+import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
+import "./AdvocaciaPage.css";
+
+const AdvocaciaPage = () => {
+  return (
+    /*<div className="advocacia-page">
+      <Container>
+        <Row>
+          <Col md={8}>
+            <h1>Advocacia de alta qualidade</h1>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+              ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+              aliquip ex ea commodo consequat.
+            </p>
+          </Col>
+          <Col md={4}>
+            <img
+              src="https://via.placeholder.com/150x150.png?text=Advogado"
+              alt="Advogado"
+            />
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            <h2>Nossos Serviços</h2>
+          </Col>
+        </Row>
+        <Row>
+          <Col md={4}>
+            <div className="service-card">
+              <h3>Direito Penal</h3>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+                do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+              </p>
+            </div>
+          </Col>
+          <Col md={4}>
+            <div className="service-card">
+              <h3>Direito Civil</h3>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+                do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+              </p>
+            </div>
+          </Col>
+          <Col md={4}>
+            <div className="service-card">
+              <h3>Direito Trabalhista</h3>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+                do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+              </p>
+            </div>
+          </Col>
+        </Row>
+      </Container>
+    </div>*/
+
+
+    
+  );
+};
+
+export default AdvocaciaPage;
